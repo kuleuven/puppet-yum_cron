9055338f50b3bac4f349447cbd84cd1cda85be0d
Yorick Poels <yorick.poels@gmail.com>
2018-10-23 15:54:43 +0200

    Add beaker - configure nodesets and project root

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,6 +1,10 @@
+require 'beaker-pe'
+require 'beaker-puppet'
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
+
+UNSUPPORTED_PLATFORMS = ['windows', 'Darwin'].freeze
 
 dir = File.expand_path(File.dirname(__FILE__))
 Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each {|f| require f}
@@ -10,6 +14,16 @@ install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
   # Readable test descriptions
   c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    hosts.each do |host|
+      copy_module_to(host, source: proj_root, module_name: 'yum_cron')
+    end
+  end
 end

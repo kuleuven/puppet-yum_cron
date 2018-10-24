a3bf6d0122d21ceb976c6102d0883a43c26ec987
Yorick Poels <yorick.poels@kuleuven.be>
2018-10-24 12:17:56 +0200

    Set config for el6 - add condition for el6/7

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,30 +1,48 @@
 # Private class
 class yum_cron::config {
 
-  file { $yum_cron::config:
-    ensure  => file,
-    owner   => 0,
-    group   => 0,
-    mode    => '0644',
-    content => epp('yum_cron/yum-cron.conf.epp'),
-    notify  => Service['yum-cron'],
+  $randomwait = fqdn_rand(300)
+
+
+  if $::operatingsystemmajrelease = '7' {
+
+    file { $yum_cron::config:
+      ensure  => file,
+      owner   => 0,
+      group   => 0,
+      mode    => '0644',
+      content => epp('yum_cron/yum-cron.conf.epp'),
+      notify  => Service['yum-cron'],
+    }
+
+    file { $yum_cron::config_cron:
+      ensure  => file,
+      owner   => 0,
+      group   => 0,
+      mode    => '0644',
+      content => epp('yum_cron/yum-cron.conf.epp'),
+      notify  => Service['yum-cron'],
+    }
+
+    file { $yum_cron::config_cron_hourly:
+      ensure  => file,
+      owner   => 0,
+      group   => 0,
+      mode    => '0644',
+      content => epp('yum_cron/0yum-hourly.cron.epp'),
+    }
   }
 
-  file { $yum_cron::config_hourly:
-    ensure  => file,
-    owner   => 0,
-    group   => 0,
-    mode    => '0644',
-    content => epp('yum_cron/yum-cron.conf.epp'),
-    notify  => Service['yum-cron'],
-  }
+  if $::operatingsystemmajrelease = '6' {
 
-  file { $yum_cron::config_cron:
-    ensure  => file,
-    owner   => 0,
-    group   => 0,
-    mode    => '0644',
-    content => epp('yum_cron/0yum-hourly.cron.epp'),
+    file { $yum_cron::config:
+      ensure  => file,
+      owner   => 0,
+      group   => 0,
+      mode    => '0644',
+      content => epp('yum_cron/yum-cron.epp'),
+      notify  => Service['yum-cron'],
+    }
   }
 
   if $::operatingsystem =~ /Scientific/ and $yum_cron::yum_autoupdate_ensure == 'disabled' {

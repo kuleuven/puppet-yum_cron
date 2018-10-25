a61a08b233fbd5aac9e9340c19bca90608c5f265
Yorick Poels <yorick.poels@kuleuven.be>
2018-10-25 14:07:51 +0200

    Remove unused contexts

--- spec/acceptance/yum_cron_spec.rb
+++ spec/acceptance/yum_cron_spec.rb
@@ -59,86 +59,42 @@ describe 'yum_cron class:' do
       end
     end
 
-    if fact('operatingsystem') =~ /Scientific/
-      describe package('yum-autoupdate') do
-        it { should be_installed }
-      end
 
-      describe file('/etc/sysconfig/yum-autoupdate') do
-        its(:content) { should match /^ENABLED=false$/ }
-      end
-    end
+  end
+end
+
+context 'when enable => false' do
+  it 'should run successfully' do
+    pp = "class { 'yum_cron': enable => false }"
+
+    apply_manifest(pp, :catch_failures => true)
+    apply_manifest(pp, :catch_changes => true)
   end
 
-  context 'with automatic updates enabled' do
-    it 'should run successfully' do
-      pp = "class { 'yum_cron': apply_updates => true }"
-
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
-    end
-
-    if fact('operatingsystemmajrelease') <= '6'
-      describe file('/etc/sysconfig/yum-cron') do
-        its(:content) { should match /^CHECK_ONLY=no$/ }
-        its(:content) { should match /^DOWNLOAD_ONLY=no$/ }
-      end
-    else
-      describe file('/etc/yum/yum-cron.conf') do
-        its(:content) { should match /^download_updates = yes$/ }
-        its(:content) { should match /^apply_updates = yes$/ }
-      end
-    end
+  describe package('yum-cron') do
+    it { should be_installed }
   end
 
-  context 'when enable => false' do
-    it 'should run successfully' do
-      pp = "class { 'yum_cron': enable => false }"
+  describe service('yum-cron') do
+    it { should_not be_enabled }
+    it { should_not be_running }
+  end
+end
 
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
-    end
+context 'when ensure => absent' do
+  it 'should run successfully' do
+    pp = "class { 'yum_cron': ensure => 'absent' }"
 
-    describe package('yum-cron') do
-      it { should be_installed }
-    end
-
-    describe service('yum-cron') do
-      it { should_not be_enabled }
-      it { should_not be_running }
-    end
+    apply_manifest(pp, :catch_failures => true)
+    apply_manifest(pp, :catch_changes => true)
   end
 
-  context 'when ensure => absent' do
-    it 'should run successfully' do
-      pp = "class { 'yum_cron': ensure => 'absent' }"
-
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
-    end
-
-    describe package('yum-cron') do
-      it { should_not be_installed }
-    end
-
-    describe service('yum-cron') do
-      it { should_not be_enabled }
-      it { should_not be_running }
-    end
+  describe package('yum-cron') do
+    it { should_not be_installed }
   end
 
-  if fact('operatingsystem') =~ /Scientific/
-    context "with yum_autoupdate_ensure => 'absent'" do
-      it 'should remove yum-autoupdate' do
-        pp = "class { 'yum_cron': yum_autoupdate_ensure => 'absent' }"
-
-        apply_manifest(pp, :catch_failures => true)
-        apply_manifest(pp, :catch_changes => true)
-      end
-
-      describe package('yum-autoupdate') do
-        it { should_not be_installed }
-      end
-    end
+  describe service('yum-cron') do
+    it { should_not be_enabled }
+    it { should_not be_running }
   end
 end
